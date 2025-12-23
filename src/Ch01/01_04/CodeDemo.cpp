274b38550cc533e1910551111c74d193142e9a6f
bunratha <bunratha94@gmail.com>
2025-12-23 08:34:46 +0000

    more dialogue

--- src/Ch01/01_04/CodeDemo.cpp
+++ src/Ch01/01_04/CodeDemo.cpp
@@ -10,7 +10,12 @@ int main(){
     std::cout << "Enter your name: " << std::flush;
     std::cin >> str;
     std::cout << "Nice to meet you, " << str << "!" << std::endl;
-
+        std::cout << "do you have a last name " << str << "?" << std::endl;
+        std::cin >> str;
+        std::cout << "Hot diggity dog that is your last name it is  " << str << "?" << std::endl;
+            std::cout << "what a cheezy last name dont ya think?";
+            std::cin >> str;
+            std::cout <<"okay okay my bad, dont mean to offend I can take  " << str << "  as an answer!" <<std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
